e7ac00b9e7e7778bfd6616d4647dc00676538ee5
Misaka10087 <m_159357@163.com>
2023-09-07 01:15:12 +0800

    规范格式

---
+++ src/components/LoginPage/MainPage.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="row" style="text-align: center; width: 100%; height: 100vh; background: url('../assets/bg.jpg');">
+        <div style="vertical-align: middle;margin: auto;">
+            <div class="col-12" style="margin-top: 20px;">
+                <img src="../assets/logo.png" alt="Logo" width="calc(50vw)">
+            </div>
+            <div class="col-12" style="margin-top: 100px;">
+                <b style="font-size: 3.5em;"> XFlySim 全球连线飞行平台</b>
+            </div>
+        </div>
+    </div>
+</template>
