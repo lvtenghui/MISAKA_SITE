e811df668efd14c7367c9cfb4d55e4f805860dbd
MISAKA18906 <2655976347@qq.com>
2023-09-06 23:25:53 +0800

    更新登录页面

---
+++ src/components/LoginPage.vue
@@ -0,0 +1,45 @@
+<template>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="../assets/logo.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+                <b> XFlySim</b>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">全球连飞地图</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">EFB在线飞行包</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="row" style="text-align: center; width: 100%; height: 100vh; background: url('../assets/bg.jpg');">
+        <div style="vertical-align: middle;margin: auto;">
+            <div class="col-12" style="margin-top: 20px;">
+                <img src="../assets/logo.png" alt="Logo" width="calc(50vw)">
+            </div>
+            <div class="col-12" style="margin-top: 100px;">
+                <b style="font-size: 3.5em;"> XFlySim 全球连线飞行平台</b>
+            </div>
+        </div>
+    </div>
+
+    <div class="row" style="text-align: center; width: 100%; height: 100vh">
+        <div style="vertical-align: middle; left: 0;right: 0;top: 5000px;bottom: 0;">
+            <div class="col-12" style="margin-top: 20px;">
+                <img src="../assets/logo.png" alt="Logo" width="calc(50vw)">
+            </div>
+            <div class="col-12" style="margin-top: 100px;">
+                <b style="font-size: 4em;"> XFlySim 全球连线飞行平台</b>
+            </div>
+        </div>
+    </div>
+</template>
