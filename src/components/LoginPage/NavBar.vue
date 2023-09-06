7ffcded0244e9170de79d47e942b72fe6ae6fd6f
Misaka10087 <m_159357@163.com>
2023-09-07 01:08:54 +0800

    修改结构

---
+++ src/components/LoginPage/NavBar.vue
@@ -0,0 +1,24 @@
+<template>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="../assets/logo.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+                <b> XFlySim</b>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">全球连飞地图</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">EFB在线飞行包</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
